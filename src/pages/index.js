921714e3f988754c0eaf4ed9f15539f51202a342
willie <willie.lee1020@outlook.com>
2021-01-30 01:24:21 -0500

    Clean readme up

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,6 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/blogposts/"> View all posts </Link>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
